911cbb1282060ef9b0d9e9dc68e878e836772784
KIMJUNSICK <jinaganda5@gmail.com>
2019-12-03 18:43:20 +0900

    Number: fraction with python

--- data_type.py
+++ data_type.py
@@ -1,4 +1,7 @@
 # Number
+import fractions
+
+# basic arithmatic operation with python
 
 print(11 / 2)  # 5.5
 
@@ -11,3 +14,12 @@ print(11 ** 2)  # 121
 
 # the rest
 print(11 % 2)  # 1
+
+# Fracition
+x = fractions.Fraction(1, 3)
+y = fractions.Fraction(2, 6)
+
+print(x)  # 1/3
+
+# python automatically reduce a fraction.
+print(y)  # 1/3
