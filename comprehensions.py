f622cbe0c8c80c49241c5e4d03bfee01965c3e0a
KIMJUNSICK <jinaganda5@gmail.com>
2019-12-06 19:34:02 +0900

    Comprehensions: file & dir > handle directory name

--- comprehensions.py
+++ comprehensions.py
@@ -9,3 +9,11 @@ import os
 current_path = os.getcwd()
 print(current_path)
 os.chdir  # change directory
+
+# handle directory name
+print(os.path.join(current_path, "junsik"))
+print(os.path.join(current_path, "\junsik"))
+print(os.path.expanduser("~"))  # path to user's home # /home/junsik
+print(
+    os.path.join(current_path, "junsik", "jeongmin", "sewon")
+)  # no limit for number of args
