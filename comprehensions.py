487a6284a9dc1358002756db51b0a0cb84c39285
KIMJUNSICK <jinaganda5@gmail.com>
2019-12-06 19:35:12 +0900

    Comprehensions: file & dir > split dir & file

--- comprehensions.py
+++ comprehensions.py
@@ -17,3 +17,13 @@ print(os.path.expanduser("~"))  # path to user's home # /home/junsik
 print(
     os.path.join(current_path, "junsik", "jeongmin", "sewon")
 )  # no limit for number of args
+
+# split file from dir
+print(os.path.split(current_path))
+# put two args into tuple at once
+current_path_plus_extension = os.getcwd() + "/conprehensions.py"
+print(current_path_plus_extension)
+(dirname, filename) = os.path.split(current_path_plus_extension)
+print(dirname)
+print(filename)
+
